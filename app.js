60a9391533b67e17e3c7121bb1b06b305a1f6b45
YuriiTrofimenko <tyaa@ukr.net>
2020-04-25 20:42:46 +0300

    try postgres

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 
 var express = require('express');
 var app = express();
+var pgp = require("pg-promise")(/*options*/);
+var db = pgp("postgres://remote_admin:1@localhost:5432/postgres");
 
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/api/', function (req, res) {
-  res.send('Hello World!');
+  db.one("SELECT $1 AS value", 123)
+    .then(function (data) {
+      console.log("DATA:", data.value);
+      res.send(`{data: ${data.value}}`);
+    })
+    .catch(function (error) {
+      console.log("ERROR:", error);
+      res.send(`{data: ${error}}`);
+    });
 });
 
 app.listen(3000, function () {
