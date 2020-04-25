f812dc867617e7aaa2d68be93bffa9e024a96ffa
YuriiTrofimenko <tyaa@ukr.net>
2020-04-25 13:35:42 +0300

    static

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/api/', function (req, res) {
   res.send('Hello World!');
 });
 
