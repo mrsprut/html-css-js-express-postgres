3b428997f1a7e79c105d0d3905ea95a406da5413
YuriiTrofimenko <tyaa@ukr.net>
2020-04-26 11:13:08 +0300

    rest product create and get all from postgres db

--- app.js
+++ app.js
@@ -5,16 +5,31 @@ var pgp = require("pg-promise")(/*options*/);
 var db = pgp("postgres://remote_admin:1@localhost:5432/postgres");
 
 app.use('/', express.static(__dirname + '/public'));
+app.use('/api', express.json());
 
-app.get('/api/', function (req, res) {
-  db.query("SELECT * FROM team")
+app.get('/api/product', function (req, res) {
+  db.query("SELECT * FROM product")
     .then(function (data) {
+      // console.log("DATA:", JSON.stringify(data));
       console.log("DATA:", JSON.stringify(data));
       res.send(`{data: ${JSON.stringify(data)}}`);
     })
     .catch(function (error) {
       console.log("ERROR:", error);
       res.send(`{data: ${error}}`);
+    });
+});
+
+app.post('/api/product', function (req, res) {
+  const newProduct = req.body;
+  console.log(`{data: ${JSON.stringify(newProduct)}}`);
+  db.none(`INSERT INTO product (title, description, price, quantity) VALUES ('${newProduct.title}', '${newProduct.description}', '${newProduct.price}', '${newProduct.quantity}')`)
+    .then(function (data) {
+      res.status(201).json({'message': 'a new product was created'});
+    })
+    .catch(function (error) {
+      console.log("ERROR:", error);
+      res.status(500).json({'error': error});
     });
 });
 
